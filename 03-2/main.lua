bbb223fc72a5c172ccddc22545b1eab1f99de927
yanuaraditia <yanuaraditia@outlook.com>
2019-03-19 10:25:16 +0700

    week 3 / 2 rev 1

--- 03-2/main.lua
+++ 03-2/main.lua
@@ -2,21 +2,32 @@ function love.load()
   anjay = love.graphics.newImage('manuk.png')
   posX = love.graphics.getWidth() / 2
   posY = love.graphics.getHeight() / 2  
+  
+  velX = 0
+  velY = 0
+  
+  desX = 0
+  desY = 0
 end
 
 function love.update()
-  if love.keyboard.isDown('up') then
-    posY = posY - 20
-  elseif love.keyboard.isDown('down') then
-    posY = posY + 20
-  elseif love.keyboard.isDown('left') then
-    posX = posX - 20
-  elseif love.keyboard.isDown('right') then
-    posX = posX + 20
+  posX = posX + velX
+  posY = posY + velY
+  
+  if posX == desX and posY == desY then
+    velX, velY = 0, 0
   end
 end
 
 function love.mousepressed(x, y)
+  desX, desY = x, y
+  velX = desX - posX
+  velY = desY - posY
+  
+  local dist = math.sqrt(math.pow(velX, 2) + math.pow(velY, 2))
+  
+  velX = velX / dist
+  velY = velY / dist
 end
 
 function love.draw()
